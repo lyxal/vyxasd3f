22334f98ae7fbb075324d0c21cef21d5c6a3d4c3
lyxal <36217120+Lyxal@users.noreply.github.com>
2025-01-12 23:17:13 +1100

    Start on making vyxasd3f work

--- lib/utils.bash
+++ lib/utils.bash
@@ -14,10 +14,10 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if vyxal3 is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
+
 
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
@@ -27,22 +27,20 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		sed 's/^v//' |
+		grep '^3\.' # Only list v3.x.x releases
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if vyxal3 has other means of determining installable versions.
 	list_github_tags
 }
+
 
 download_release() {
 	local version filename url
 	version="$1"
 	filename="$2"
-
-	# TODO: Adapt the release URL convention for vyxal3
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$GH_REPO/releases/download/v${version}/vyxal-${version}.jar"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +59,9 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert vyxal3 executable exists.
+		echo '#!/bin/bash
+java -jar "$(dirname "$0")/vyxal3.jar" "$@"' > vyxal && chmod +x vyxal
+
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
